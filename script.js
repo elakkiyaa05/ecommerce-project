1c08d9ec81fecc634d311e8eff42569c5804df73
elakkiyaa05 <elakkiyaamani0505@gmail.com>
2026-05-29 21:55:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // PRODUCTS
-// const products = [
-//  {
-//    id: 1,
-//     name: "Running Shoes",
-//     price: 1499,
-//     image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
-//   },
-//   {
-//     id: 2,
-//     name: "Smart Watch",
-//     price: 2999,
-//     image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30"
-//   },
-//   {
-//     id: 3,
-//     name: "Headphones",
-//     price: 1999,
-//     image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
-//   }
-// ];
+ const products = [
+  {
+    id: 1,
+     name: "Running Shoes",
+     price: 1499,
+     image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
+   },
+   {
+     id: 2,
+     name: "Smart Watch",
+     price: 2999,
+     image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30"
+   },
+   {
+     id: 3,
+     name: "Headphones",
+     price: 1999,
+     image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
+ }
+ ];
 let products = [];
 
 fetch("http://localhost:3000/products")
